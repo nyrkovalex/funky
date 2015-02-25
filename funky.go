739d16602d849fca80d500a4ae6c921e4c060874
Alexander Nyrkov <nyrkovalex@gmail.com>
2015-02-25 23:29:08 +0300

    named result params

--- funky.go
+++ funky.go
@@ -9,34 +9,31 @@ type Slice []interface{}
 // Filter filters the slice sequentually applying predicate
 // function provided to each slice element.
 // Returns slice containing elements for which fn predicate returned true.
-func (s Slice) Filter(fn func(interface{}) bool) Slice {
-	var result Slice
+func (s Slice) Filter(fn func(interface{}) bool) (result Slice) {
 	for _, item := range s {
 		if fn(item) {
 			result = append(result, item)
 		}
 	}
-	return result
+	return
 }
 
 // Map applies function provided to each element of slice and returns new
 // Slice containing elements returned by mapping function fn.
-func (s Slice) Map(fn func(interface{}) interface{}) Slice {
-	var result Slice
+func (s Slice) Map(fn func(interface{}) interface{}) (result Slice) {
 	for _, item := range s {
 		result = append(result, fn(item))
 	}
-	return result
+	return
 }
 
 // Reduce reduces Slice s with the fn function provided to a singe value.
 // Value returned by previous fn call is passed as its first argument on the next run.
-func (s Slice) Reduce(fn func(interface{}, interface{}) interface{}) interface{} {
-	var collected interface{}
+func (s Slice) Reduce(fn func(interface{}, interface{}) interface{}) (collected interface{}) {
 	for i := 1; i < len(s); i++ {
 		collected = fn(s[i-1], s[i])
 	}
-	return collected
+	return
 }
 
 // Append returns new Slice containing elements from Slice s and items
